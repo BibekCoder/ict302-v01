6b58dc05f0588b70072f015114102116b6849a26
CuriousSoul <bibekbamjan405@gmail.com>
2026-01-01 10:55:50 +1100

    installed node modules for frontend and reorganized files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { StrictMode } from "react";
+import {strictMode} from "react";
+import {createRoot} from "react-dom/client";
 import ReactDOM from "react-dom/client";
 import "./styles.css";
-import App from "./App.jsx";
+import App from "./app.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
