d1d98bfee500f9e063b74a075ee7f816e6a57d9a
Aaron <20028022@students.koi.edu.au>
2026-01-05 22:27:43 +1100

    libraries

--- ict302-backend-v1/routes/auth.routes.js
+++ ict302-backend-v1/routes/auth.routes.js
@@ -9,7 +9,7 @@ const adminOnly = [authMiddleware, roleMiddleware(["admin"])];
 const authController = require("../controllers/auth.controller");
 
 // Register user 
-router.post( "/register", adminOnly, authController.register);
+router.post( "/register", authController.register);
 router.post( "/login", authController.login);
 module.exports = router;
 
